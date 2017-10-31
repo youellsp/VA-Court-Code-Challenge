4e724a09818a27c1aab89967fa672d04356c8311
patrickyouells <youells.patrick@gmail.com>
2017-10-31 13:53:33 -0400

    Adding project to GitHub

--- src/app/component/challenge-four/challenge-four.component.ts
+++ src/app/component/challenge-four/challenge-four.component.ts
@@ -12,4 +12,19 @@ export class ChallengeFourComponent implements OnInit {
   ngOnInit() {
   }
 
+  questionText =
+    "\n" +
+    " $(document).ready(function() {\n" +
+    "    $(\"#groupsBTN\").click(function(event){\n" +
+    "       $.getJSON('/instrument/groups?action=getAll, function(response, status) {\n" +
+    "          if (status === 200) {\n" +
+    "            var groupList = response.data;\n" +
+    "            $.each(groupList, function(group) {\n" +
+    "              $('#groupList').append('<li> Instrument Group: ' + group.name + '</li>');\n" +
+    "            })\n" +
+    "          }\n" +
+    "       });\n" +
+    "    });\n" +
+    " });"
+
 }
